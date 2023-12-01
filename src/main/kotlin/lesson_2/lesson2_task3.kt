c1135fe03057c8c148527dfff4afc11ad79fc1ac
MSrescuer <sinka1696@gmail.com>
2023-12-01 12:59:12 +0700

    KS-2-3 Новая переменная, убрал магическое число.

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,10 +4,11 @@ fun main() {
     val timeHour = 9
     val timeMinute = 39
     val travelTime = 457
+    val minutesInHour = 60
 
-    val totalTimeMinute = timeHour * 60 + timeMinute
-    val arrivalTime = (travelTime + totalTimeMinute) / 60
-    val arrivalTime1 = (travelTime + totalTimeMinute) % 60
+    val totalTimeMinute = timeHour * minutesInHour + timeMinute
+    val arrivalTime = (travelTime + totalTimeMinute) / minutesInHour
+    val arrivalTime1 = (travelTime + totalTimeMinute) % minutesInHour
 
     println("Время прибытия: $arrivalTime:$arrivalTime1")
 
