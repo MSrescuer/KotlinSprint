4c6f7518f8347fdf92cdb3cedb78c00ec3cc2929
MSrescuer <sinka1696@gmail.com>
2023-12-04 23:39:32 +0700

    KS-2-5 Добавил константу.

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,12 +1,14 @@
 package lesson_2
 
 import kotlin.math.pow
+const val PERCENT = 100
 
 fun main() {
     val sum = 70_000
-    val percent = 16.7
+    val annual = 16.7
 
-    val result = sum * (1 + percent / 100).pow(20)
+
+    val result = sum * (1 + annual / PERCENT).pow(20)
 
     println(String.format("Размер вклада через 20 лет: %.3f.", result))
 }
