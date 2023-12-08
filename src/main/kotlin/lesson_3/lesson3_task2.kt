92b62827e91b0baf0e000cf52b86e2a172731600
MSrescuer <sinka1696@gmail.com>
2023-12-08 18:29:37 +0700

    KS-3-2 Смена фамилии.

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,11 @@
+package lesson_3
+
+fun main() {
+    val name = "Андреева Татьяна Сергеевна"
+    val name1 = "Сидорова Татьяна Сергеевна"
+    val age = 20
+    val age1 = 22
+
+    println("$name, $age")
+    println("$name1, $age1")
+}
